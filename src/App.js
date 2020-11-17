1d25ba4c46d7f968a7669007e2e1ed5f14aaaad1
varikhryn <y.yarikhryn@gmail.com>
2020-11-17 13:39:22 +0200

    simple shop cart on React(OOP)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import Cart from "./Cart";
+import Goods from "./Goods";
+import goodsArr from "./goods.json";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cart: {},
+      count: 0,
+    }
+  }
+
+  addToCart = (event) => {
+    event.preventDefault();
+    if (!event.target.classList.contains('add-to-cart')) return false;
+    let cartTemp = this.state.cart;
+    cartTemp[event.target.dataset.key] ? cartTemp[event.target.dataset.key]++ : cartTemp[event.target.dataset.key] = 1;
+    // cartTemp++;
+    this.setState({ cart: cartTemp });
+    let count = this.state.count;
+    count++;
+    this.setState({ 'count': count })
+  }
+
+
+  render() {
+    let showCart;
+    if (this.state.count !== 0) {
+      showCart = <Cart cart={this.state.cart} goods={goodsArr} />;
+    }
+    else {
+      showCart = 'Empty';
+    }
+    return (
+      <div className="container">
+        <h1>Cart</h1>
+        <div className="goods-field" onClick={this.addToCart}>
+          {goodsArr.map(item => <Goods title={item.title} cost={item.cost} image={item.image}
+            articul={item.articul} key={item.articul} />)}
+        </div>
+        {showCart}
+
+      </div>
+    );
+  }
 }
 
 export default App;
